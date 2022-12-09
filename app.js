e89ae5d373936cf745a81d0c875a10ec15686df2
School Of Code <isabellaRebecca@live.co.uk>
2022-12-09 15:39:58 +0000

    C:/Program Files/Git/

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ import express from "express";
 // import morgan from "morgan";
 import  petsRouter  from "./Routes/PetsRoutes.js";
 import  ownersRouter  from "./Routes/OwnersRoutes.js";
-// import cors from "cors";
+import cors from 'cors'; // import cors package 
 
 export const app = express();
 const PORT = process.env.port || 3000;
-
+app.use(cors());
 // app.use(express.static("public")); not needed???
 // app.use(cors());
 app.use(express.json());
